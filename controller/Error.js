716691e9cd101e55940cd48506fe9cbed3074e26
Bhushan Joshi <bhushanjoshi911@gmail.com>
2020-07-22 20:40:08 +0530

    ~

---
+++ controller/Error.js
@@ -0,0 +1,15 @@
+exports.get404 = (req, res, next) => {
+    res.status(404).render('404', {
+        path: null,
+        title: 'pageNotFound', 
+        isAuthenticted: req.session.isLoggedin
+    });
+};
+
+exports.get500 = (req, res, next) => {
+    res.status(500).render('500', {
+        path: null, 
+        title: 'pageNotFound', 
+        isAuthenticted: req.session.isLoggedin
+    });
+};
